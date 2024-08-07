feaf8335a02c31513b1b6c288265635e5821d309
koonnuthhh <157001558+6631503001@users.noreply.github.com>
2024-08-07 23:14:45 +0700

    make rule look better fix: make it change instance when mobile change choice

--- ChangeQuestion.js
+++ ChangeQuestion.js
@@ -4,47 +4,58 @@ function QuestionforGroup(GroupName) {
 function RealQuestion(Text) {
     document.getElementById("Question").innerHTML = Text;
 }
-document.addEventListener('click', function () {
+document.getElementById("select-group").addEventListener('change', function () {
     let GroupNum = document.getElementById("select-group").value;
     let GroupName = document.getElementById(GroupNum).innerHTML;
     let Title = document.getElementById("Title");
-    if(GroupNum != 0) {
-        document.getElementById("Base choice").delete;
-    }
     switch (GroupNum) {
+        case "0":
+            Title.innerHTML = "There is no question here";
+            RealQuestion("I'm just too lazy to delete this choice");
+            break;
         case "1":
             QuestionforGroup(GroupName);
             RealQuestion("Phatpong is live near Rangsit. He want to meet his friend at Sala Daeng. How much it take to go there and how much time it take?");
             break;
         case "2":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "3":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "4":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "5":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "6":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "7":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "8":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "9":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "10":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
         case "11":
             QuestionforGroup(GroupName);
+            RealQuestion();
             break;
     }
 });
