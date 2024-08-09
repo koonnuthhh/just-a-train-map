0374ad6ad4430869e955b66e2dd3a81a0c160a4c
koonnuthhh <157001558+6631503001@users.noreply.github.com>
2024-08-09 14:03:25 +0700

    Add question

--- ChangeQuestion.js
+++ ChangeQuestion.js
@@ -15,19 +15,19 @@ document.getElementById("select-group").addEventListener('change', function () {
             RealQuestion("I'm just too lazy to delete this choice");
             break;
         case "1":
-            QuestionforGroup(GroupName,"Phatpong is live near Rangsit. He want to meet his friend at Sala Daeng. How much it take to go there if he want to go to Nonthaburi civic center first? Tell me the fastest way and the longest");
+            QuestionforGroup(GroupName,"Phatpong is live near Rangsit. He want to meet his friend at Sala Daeng. How much it take to go there if he want to go to Nonthaburi civic center first? Tell me the cheapest way and the most expensive way.");
             break;
         case "2":
-            QuestionforGroup(GroupName);
+            QuestionforGroup(GroupName,"Phatpong is live near Rangsit. He want to meet his friend at Huai Khwang. How much time it take to go there if he want to go to Phra Ram 9 and Phetchaburi first? Tell me the fastest way and the longest way.**You can't use regular train**");
             break;
         case "3":
-            QuestionforGroup(GroupName);
+            QuestionforGroup(GroupName,"Ohm just arrive at Rangsit. He want to travel around the city by railway. How much time and money it take to travel around the city if he just want to know cityside? Tell me the cheapest way and the most expensive way.");
             break;
         case "4":
-            QuestionforGroup(GroupName);
+            QuestionforGroup(GroupName,"Ohm just arrive at Rangsit. He want to travel around the city by railway. How much time and money it take to travel around the city if he just want to know all the railway station? Tell me the cheapest way and the most expensive way.");
             break;
         case "5":
-            QuestionforGroup(GroupName);
+            QuestionforGroup(GroupName,"Sathu is at Wat Phra Si Mahathat.**Need to add more after this**");
             break;
         case "6":
             QuestionforGroup(GroupName);
@@ -39,7 +39,7 @@ document.getElementById("select-group").addEventListener('change', function () {
             QuestionforGroup(GroupName);
             break;
         case "9":
-            QuestionforGroup(GroupName);
+            QuestionforGroup(GroupName,);
             break;
         case "10":
             QuestionforGroup(GroupName);
